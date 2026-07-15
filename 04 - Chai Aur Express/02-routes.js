fd00a5a993421f93fed384db187db5c527e66e57
mansiey <mansi46rd@gmail.com>
2026-07-15 14:02:01 +0530

    done with the routes

--- 04 - Chai Aur Express/02-routes.js
+++ 04 - Chai Aur Express/02-routes.js
@@ -47,7 +47,66 @@ function block1_httpMethods(){
     })
 
 
+    //Put method to replace the data
+    app.put('/routes/:id', (req, res) => {
+        const id = req.params.id;
+        if(!routes[id]){
+            return res.status(404).json({error: 'Something went wrong'});
+        }
+
+        routes[id] = {id: Number(id), ...req.body};
+
+        res.status(200).json(routes[id]);
+    })
+
+    //Patch to update the data
+    app.patch('/routes/:id', (req, res) => {
+        const id = req.params.id;
+
+        if(!routes[id]){
+            return res.status(404).json({error: "something went wrong!"});
+        }
+
+        routes[id] = {...routes[id], ...req.body};
+
+        res.status(200).json(routes[id]);
+    })
+
+    //delete method to delete the route
+    app.delete('/routes/:id', (req, res) => {
+        const id = req.params.id;
+        if(!routes[id]){
+            return res.status(404).json({Error: "routes was not found"});
+        }
+
+        delete routes[id];
+
+        // res.status(200).json({message: "Deleted Successfully!"});
+        res.status(204).end();       //better 
+    }) 
+
     const server = app.listen(0, async () => {
-        
+        const PORT = server.address().port;
+        const base = `http://127.0.0.1:${base}`;
+
+        const listRes = await fetch(`${base}/routes`);
+        const listData = await listRes.json();
+        console.log('All the trains are :', JSON.stringify(listData));
+        console.log('++++++++++++++++++++++++')
+
+
+        const createRes = await fetch(`${base}/routes`, {
+            method: POST,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                name: 'Dadar-Wadala local',
+                direction: 'East'
+            }
+        });
+
+        const createData = await createRes.json();
+        console.log('Added Route: ', JSON.stringify(createData));
     })
 }
